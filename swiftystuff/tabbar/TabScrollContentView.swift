392fbc0d74889defcec5b97b0869c1fcc293a03e
abdulrahimiliasu <abdulrahimiliasu@icloud.com>
2024-05-04 21:07:09 +0200

    added tab content view

--- swiftystuff/tabbar/TabScrollContentView.swift
+++ swiftystuff/tabbar/TabScrollContentView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct TabScrollContentView: View {
+    let tab: Tabs
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVStack {
+                    ForEach((1 ... 3).reversed(), id: \.self) { _ in
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(tab.item.color.opacity(0.5))
+                            .frame(height: 180)
+                            .scrollTransition { content, phase in
+                                content
+                                    .scaleEffect(phase.isIdentity ? 1 : 0.8)
+                                    .blur(radius: phase.isIdentity ? 0 : 10)
+                            }
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(tab.item.title)
+        }
     }
 }
 
 #Preview {
-    TabScrollContentView()
+    TabScrollContentView(tab: .home)
 }
