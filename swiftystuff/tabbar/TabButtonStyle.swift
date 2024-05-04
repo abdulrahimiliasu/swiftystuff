1985dda5e7060466f976a6677efb46c85733140e
abdulrahimiliasu <abdulrahimiliasu@icloud.com>
2024-05-04 21:06:48 +0200

    added tab button styles

--- swiftystuff/tabbar/TabButtonStyle.swift
+++ swiftystuff/tabbar/TabButtonStyle.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct TabButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 0.9 : 1)
+            .opacity(configuration.isPressed ? 0.9 : 1)
+    }
+}
+
+extension View {
+    func withTabButtonStyle() -> some View {
+        buttonStyle(TabButtonStyle())
+    }
+}
