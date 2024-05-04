e4dd96513d5b7d2b7a7be31289b35ff65fe1589c
abdulrahimiliasu <abdulrahimiliasu@icloud.com>
2024-05-04 21:06:18 +0200

    add model - tabs - tab item

--- swiftystuff/tabbar/TabItemModel.swift
+++ swiftystuff/tabbar/TabItemModel.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct TabItem: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let systemImage: String
+    let color: Color
+}
--- swiftystuff/tabbar/Tabs.swift
+++ swiftystuff/tabbar/Tabs.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+enum Tabs: CaseIterable {
+    case home, discover, profile, settings
+    var item: TabItem {
+        switch self {
+            case .home:
+                .init(title: "Home", systemImage: "house", color: .blue)
+            case .discover:
+                .init(title: "Discover", systemImage: "sparkles", color: .red)
+            case .profile:
+                .init(title: "Profile", systemImage: "person", color: .purple)
+            case .settings:
+                .init(title: "Settings", systemImage: "gear", color: .orange)
+        }
+    }
+}
