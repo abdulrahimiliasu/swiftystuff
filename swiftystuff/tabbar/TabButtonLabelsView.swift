3247a674e82d83e09b197d9f3e5709628b33eb3b
abdulrahimiliasu <abdulrahimiliasu@icloud.com>
2024-05-04 21:07:32 +0200

    added tab button labels

--- swiftystuff/tabbar/TabButtonLabelsView.swift
+++ swiftystuff/tabbar/TabButtonLabelsView.swift
@@ -7,12 +7,35 @@
 
 import SwiftUI
 
-struct TabButtonLabelsView: View {
+let BTTN_HEIGHT: CGFloat = 50
+let CORNER_RADIUS: CGFloat = 35.0
+
+struct ActiveTabLabel: View {
+    let tabItem: TabItem
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: CORNER_RADIUS)
+                .fill(tabItem.color.opacity(0.1))
+                .shadow(color: .primary.opacity(0.5), radius: 3, x: 0, y: 5)
+                .frame(width: 120, height: BTTN_HEIGHT)
+            Label(tabItem.title, systemImage: tabItem.systemImage)
+                .labelStyle(.titleAndIcon)
+        }
     }
 }
 
-#Preview {
-    TabButtonLabelsView()
+struct InActiveTabLabel: View {
+    let tabItem: TabItem
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: CORNER_RADIUS)
+                .fill(.clear)
+                .frame(width: 50, height: BTTN_HEIGHT)
+            Label(tabItem.title, systemImage: tabItem.systemImage)
+                .labelStyle(.iconOnly)
+                .symbolEffectsRemoved()
+        }
+    }
 }
